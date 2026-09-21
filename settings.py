a198ce643c6b1cd082600468546d3ae32ed4768b
malid1600-stack <malid1600@gmail.com>
2026-09-21 18:39:11 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'main' / 'static',
