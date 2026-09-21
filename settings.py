87c59c87c3a7a0109da414c30578ec64de20f17b
malid1600-stack <malid1600@gmail.com>
2026-09-21 19:34:45 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -123,6 +123,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
